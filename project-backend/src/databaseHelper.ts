c44a315273ca7256e0444d19b3ec4b7125ce79e8
James West <jameswwest20@gmail.com>
2025-03-25 01:17:37 +1100

    database set up properly and some login routes done

--- project-backend/src/databaseHelper.ts
+++ project-backend/src/databaseHelper.ts
@@ -1,5 +1,7 @@
 import path from "path";
 import sqlite3 from "sqlite3";
+import {v4 as uuidv4} from 'uuid';
+import { User } from "./interface";
 
 
 
@@ -20,7 +22,7 @@ db.run('PRAGMA foreign_keys = ON;', (err) => {
 const createTable = () => {
     db.serialize(() => {
         db.run(`CREATE TABLE IF NOT EXISTS users(
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                id INTEGER PRIMARY KEY,
                 email TEXT NOT NULL,
                 nameFirst TEXT NOT NULL,
                 nameLast TEXT NOT NULL,
@@ -28,7 +30,7 @@ const createTable = () => {
                 password STRING NOT NULL
             )`);
         db.run(`CREATE TABLE IF NOT EXISTS societies (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                id INTEGER PRIMARY KEY,
                 name TEXT NOT NULL,
                 description TEXT NOT NULL,
                 arc TEXT NOT NULL,
@@ -52,4 +54,73 @@ const createTable = () => {
     })
 }
 
-export {db, createTable};
+const createUser = (email:string, nameFirst: string, nameLast:string, zid:number, password:string): Promise<any> => {
+    return new Promise((resolve, reject) => {
+        const id = Math.floor(Math.random() * 1000000000);
+        const sql: string = `INSERT INTO users (email, nameFirst, nameLast, zid, password, id) VALUES (?, ?, ?, ?, ?, ?)`;
+        db.run(sql, [email, nameFirst, nameLast, zid, password, id], (err) => {
+            if (err) {
+                reject(err);
+            }
+            else {
+                resolve({authUserId: id});
+            }
+        });
+    })
+}
+
+const getUser = (userId:number) => {
+    return new Promise((resolve, reject) => {
+        const sql: string = `SELECT * from users WHERE id = ?`;
+        db.get(sql, [userId], (err, row) => {
+            if (err) {
+                reject(err);
+            }
+            else {
+                resolve(row)
+            }
+        })
+    });
+}
+
+const attemptLogin = (email:string, password: string): Promise<User> => {
+    return new Promise((resolve, reject) => {
+        const sql: string = `SELECT * from users WHERE email = ? AND password = ?`;
+        db.get(sql, [email, password], (err, row) => {
+            if (err) {
+                reject(err);
+            }
+            else {
+                resolve(row as User);
+            }
+        })
+    })
+}
+const createSoc = (name:string, description: string, arc: string, facebook: string, website: string) => {
+    return new Promise((resolve, reject) => {
+        const id = Math.floor(Math.random() * 1000000000);
+        const sql: string = `INSERT INTO societies (name, description, arc, facebook, website, id) VALUES (?, ?, ?, ?, ?, id)`;
+        try {
+            resolve({societyId: id});
+        }catch (e) {
+            reject(e);
+        }
+        db.run(sql, [name, description, arc, facebook, website, id]);
+    })
+}
+
+const getSoc = (id:number) => {
+    return new Promise((resolve, reject) => {
+        const sql: string = `SELECT * from societies WHERE id = ?`;
+        db.get(sql, [id], (err, row) => {
+            if (err) {
+                reject(err);
+            }
+            else {
+                resolve(row)
+            }
+        })
+    });
+}
+
+export {db, createTable, createUser, createSoc, getUser, attemptLogin};
--- project-backend/src/server.ts
+++ project-backend/src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
-import {createTable} from './databaseHelper.js';
+import {createTable, createUser, getUser, attemptLogin} from './databaseHelper.js';
+import { User } from './interface.js';
 createTable();
 const app = express();
 
@@ -7,13 +8,54 @@ app.get('/', function (req: express.Request, res: express.Response) {
     res.send('Hello World');
 })
 
-
-// app.put('/subscribe', (req:express.Request, res:express.Response) => {
-// })
-
-
+app.get('/createuser', async (req, res) => {
+    const email:string = req.body.email;
+    const nameFirst:string = req.body.nameFirst;
+    const nameLast:string = req.body.nameLast;
+    const zid:number = req.body.zid;
+    const password:string = req.body.password;
+    try {
+        const uid = await createUser(email, nameFirst, nameLast, zid, password);
+        res.status(200).send({userId: uid});
+        console.log(uid);
+    } catch (e) {
+        res.status(400).send(e.message);
+    }
+})
+app.get('/getuser', async (req, res) => {
+    try {
+        const users = await getUser(req.body.authUserId);
+        res.status(200).send(users);
+    } catch(e) {
+        res.status(400).send("boo");
+    }
+})
+app.post('/auth/register', async (req, res) => {
+    try {
+        let zId: number = req.body.zId;
+        let email:string = req.body.email;
+        let password:string = req.body.password;
+        let nameFirst:string = req.body.nameFirst;
+        let nameLast:string = req.body.nameLast;
+        const authUserId = await createUser(email, nameFirst, nameLast, zId, password);
+        res.status(200).send({authUserId: authUserId});
+    } catch(e) {
+        res.status(400).send({error: e.message});
+    }
+})
+app.get('/auth/login', async (req, res) => {
+    try {
+        const userObj: User = await attemptLogin(req.body.email, req.body.password);
+        res.status(200).send({authUserId: userObj.authUserId});
+    } catch (e) {
+        res.status(400).send({error: e.message});
+    }
+})
+app.delete('/auth/logout', async (req, res) => {
+    
+})
 app.listen(3000, () => {
-    console.log("listening")
+    console.log("listening");
 });
 
 export default app;
