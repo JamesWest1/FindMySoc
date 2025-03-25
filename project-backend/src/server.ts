e736753069c2ff5a70c90f9bc0c2bb348c5357df
James West <jameswwest20@gmail.com>
2025-03-25 12:31:00 +1100

    src/server.ts

--- project-backend/src/server.ts
+++ project-backend/src/server.ts
@@ -45,7 +45,7 @@ app.post('/auth/register', async (req, res) => {
 })
 app.get('/auth/login', async (req, res) => {
     try {
-        const userObj: User = await attemptLogin(req.body.email, req.body.password);
+        const userObj = await attemptLogin(req.body.email, req.body.password);
         res.status(200).send({authUserId: userObj.authUserId});
     } catch (e) {
         res.status(400).send({error: e.message});
@@ -53,6 +53,22 @@ app.get('/auth/login', async (req, res) => {
 })
 app.delete('/auth/logout', async (req, res) => {
     
+})
+
+app.get('/socities/all', async (req, res) => {
+    res.status(200).send({societies: []}) // array of Society
+})
+app.get('/society/subscribed', async (req, res) => {
+    const userId:number = req.body.authUserId;
+    res.status(200).send({societies: []}) // array of Society (that userId is a part of)
+})
+app.get('/events/future', async (req, res) => {
+    const userId: number = req.body.authUserId;
+    res.status(200).send({events: []}) // array of Event (from socities that the user is a part of and are in the future)
+})
+app.get('/events/past', async (req, res) => {
+    const userId: number = req.body.authUserId;
+    res.status(200).send({events: []}) // array of Event (from socities that the user is a part of and have already passed)
 })
 app.listen(3000, () => {
     console.log("listening");
